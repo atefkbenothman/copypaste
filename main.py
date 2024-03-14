d42905ed9233cde88587dd9cf8c6a905e0107842
atefkbenothman <atefkbenothman@gmail.com>
2024-03-13 21:42:48 -0700

    refactor

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ import pyperclip
 
 DIR_EXCLUDE_LIST = [
   "venv",
-  "node_modules"
+  "node_modules",
+  ".git"
 ]
 
 FILE_INCLUDE_LIST = [
@@ -32,33 +33,30 @@ def walk_directory(dir_path: str) -> list[str]:
   """
   walk the directory and return list of files
   """
-  all_files = []
+  file_paths = []
   for root, dirs, files in os.walk(dir_path):
     dirs[:] = [d for d in dirs if d not in DIR_EXCLUDE_LIST]
     for file in files:
       file_extension = file.split(".")[-1]
       if file_extension in FILE_INCLUDE_LIST:
-        all_files.append(os.path.join(root, file))
-  return all_files
+        file_paths.append(os.path.join(root, file))
+  return file_paths
 
 
 def combine_files(files: list[str], dir_path: str) -> str:
   """
   combine contents of all files into one giant string
   """
-  root_dir_idx = dir_path.rfind("/")
   combined_content = ""
-  line_count = 0
+  root_dir_idx = dir_path.rfind("/")
   for file in files:
-    combined_content += "-" * 20 + "\n"
-    combined_content += f"file name: {file[root_dir_idx + 1:]}" + "\n\n"
     with open(file, "r") as f:
-      for line in f:
-        combined_content += line
-        line_count += 1
+      combined_content += "-" * 20 + "\n"
+      combined_content += f"file name: {file[root_dir_idx + 1:]}" + "\n\n"
+      combined_content += f.read()
       combined_content += "\n"
   pyperclip.copy(combined_content)
-  print(f"copied contents from {len(files)} files to clipboard (total lines ~= {line_count})")
+  print(f"copied contents from {len(files)} files to clipboard")
   return combined_content
 
 
@@ -68,20 +66,15 @@ def combine_files_to_new_file(files: list[str], output_file: str, dir_path: str)
   """
   if os.path.exists(output_file):
     os.remove(output_file)
-  line_count = 0
   root_dir_idx = dir_path.rfind("/")
   with open(output_file, "a") as out_file:
     for file in files:
-      out_file.write("-" * 20 + "\n")
-      out_file.write(f"file name: {file[root_dir_idx + 1:]}" + "\n\n")
       with open(file, "r") as in_file:
-        for line in in_file:
-          out_file.write(line)
-          line_count += 1
-      out_file.write("\n\n")
-      out_file.write("-" * 20 + "\n")
-      line_count += 3
-  print(f"combined {len(files)} files into {output_file} with {line_count} lines")
+        out_file.write("-" * 20 + "\n")
+        out_file.write(f"file name: {file[root_dir_idx + 1:]}" + "\n\n")
+        out_file.write(in_file.read())
+        out_file.write("\n\n")
+  print(f"combined {len(files)} files into {output_file}")
 
 
 if __name__ == "__main__":
